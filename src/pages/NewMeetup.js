7b28409c93d84ee54275f63fceae315db2f69cdf
Milad Moulayi <m.moulayi123@gmail.com>
2023-06-04 18:24:07 -0700

    Added POST to NewMeetupForm

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,30 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  return <NewMeetupForm />;
+  const history = useHistory();
+
+  function handleAddMeetup(meetupData) {
+    fetch(
+      'https://react-getting-started-cda22-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={handleAddMeetup} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
